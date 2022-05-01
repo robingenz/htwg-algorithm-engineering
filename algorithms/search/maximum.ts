a41ba25702a720a75dfcd3be4c1dce9b869cebb1
Robin Genz <mail@robingenz.dev>
2022-05-01 08:58:06 +0000

    refactor: use typescript

---
+++ algorithms/search/maximum.ts
@@ -0,0 +1,14 @@
+const maxElementIndex = (values: number[]): number => {
+  let indexMaxValue = 0;
+  let maxValue = values[0];
+  for (let index = 0; index < values.length; index++) {
+    if (values[index] > maxValue) {
+      indexMaxValue = index;
+      maxValue = values[index];
+    }
+  }
+  return indexMaxValue;
+};
+
+const result = maxElementIndex([1720, 1721, 979, 366, 299, 675, 1456]);
+console.log(`Index: ${result}`);
