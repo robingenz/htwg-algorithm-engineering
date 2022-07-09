73b28eccbdaa1b80f7509b9f2273b914879832cb
Robin Genz <mail@robingenz.dev>
2022-07-09 15:13:38 +0000

    feat(divide-and-conquer): add `potentiate` algorithm

---
+++ algorithms/divide-and-conquer/potentiate.py
@@ -0,0 +1,20 @@
+"""
+Potenzieren
+
+Berechnung der Potenz der Basis b und des Exponenten e.
+
+Komplexität: 
+"""
+
+
+def main(b: int, e: int) -> int:
+    if e == 0:
+        return 1
+    elif e % 2 == 0:
+        return main(b, int(e/2)) * main(b, int(e/2))
+    else:
+        return b * main(b, int(e/2)) * main(b, int(e/2))
+
+
+if __name__ == "__main__":
+    print(main(9, 3))
